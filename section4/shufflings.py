1a824169cd43b74342d244c76d2f0ab34df48ec8
noobxzx <148211453+noobxzx@users.noreply.github.com>
2024-02-19 12:32:08 +0200

    update shuffle 2

--- section4/shufflings.py
+++ section4/shufflings.py
@@ -1,6 +1,14 @@
-import random
+import random 
 from random_lists_gen import large_list_gen, medium_list_gen, small_list_gen
 
+#sort 1
+def dict_generator(lst): 
+    return {element: random.random() for element in lst}
+
+def sort_by_value(dictionary):
+    return sorted(dictionary.keys(), key = lambda x: dictionary[x])
+
+# sort 2 
 def knuth_shuffle(lst):
     for i in range(len(lst) - 1):
         k = random.randint(0, len(lst) - 1)
@@ -11,6 +19,13 @@ if __name__ == "__main__":
     small_list = small_list_gen()
     medium_list = medium_list_gen()
     large_list = large_list_gen()
+
+    small_dict = {}
+    print(sort_by_value(dict_generator(small_list)))
+    medium_dict= {}
+    print(sort_by_value(dict_generator(medium_list)))
+    large_dict= {}
+    print(sort_by_value(dict_generator(large_list)))
 
     print(knuth_shuffle(small_list))
     print(knuth_shuffle(medium_list))
