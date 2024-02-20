17df691efc1b16e5c4bbe4b3e46e4c1b17b4ed35
llll <dyakonenko123@gmail.com>
2024-02-20 13:25:07 +0200

    add logs

---
+++ section4/add_logs.py
@@ -0,0 +1,32 @@
+import os, re
+
+def add_data_to_log(data_dict, sort_label):
+    next_number = 1
+    while os.path.exists(f"section4/logs/log-{sort_label}-{next_number}.txt"):
+        next_number += 1
+        
+    with open(f"section4/logs/log-{sort_label}-{next_number}.txt", "w") as file :
+        file.write(f"·:*¨༺ ♱✮♱ ༻¨*:·\n") 
+        file.write(f"  ╱|、\n(˚ˎ 。7  \n |、˜〵  \nじしˍ,)ノ:·\n\n") 
+
+        flattened_data = []
+        for key, value in data_dict.items():
+            for size, times in value.items():
+                avg_time = sum(times) / len(times)
+                flattened_data.append((key, size, avg_time, times))
+
+        # Sort the data based on the average time
+        sorted_data = sorted(flattened_data, key=lambda x: x[2])
+
+        # Write the sorted data into the text file
+        for key, size, avg_time, experiments in sorted_data:
+            file.write(f"૮₍ ˃ ⤙ ˂ ₎ა¨*:·\n")   
+            file.write(f"{key} - size: {size}, average Time: {avg_time}\n")
+            file.write("experiments:\n")
+            for i, time in enumerate(experiments, 1):
+                file.write(f"experiment {i}: {time}\n")
+            file.write(f"·:*¨༺ ♱✮♱ ༻¨*:·\n\n")  
+
+if __name__ == '__main__':
+    # add_data_to_log(data1, 'section4/logs.txt')
+    pass
--- section4/test_sorting.py
+++ section4/test_sorting.py
@@ -1,4 +1,4 @@
-import time, random_lists_gen, shufflings
+import time, random_lists_gen, shufflings, add_logs
 from sortings import InsertionSort, SelectionSort, ShellSort
 
 def sort_time_counter(sorting_function, input_list, size):
@@ -10,12 +10,11 @@ def sort_time_counter(sorting_function, input_list, size):
 
 def experimenting(sorting_function, custom_list_dict):
     results = {}
-
     for list_gen_func, size_tests_dict in custom_list_dict.items():
         results[list_gen_func.__name__] = {}
         for size, tests in size_tests_dict.items():
             results[list_gen_func.__name__][size] = []
-            for i in range(tests):
+            for _ in range(tests):
                 time_taken = sort_time_counter(sorting_function, list_gen_func(size), size)
                 results[list_gen_func.__name__][size].append(time_taken)
 
@@ -31,6 +30,7 @@ if __name__ == "__main__":
         random_lists_gen.almost_identical_list: {1000: 5}
     }
 
-    print(f"{experimenting(InsertionSort.insertion_sort, custom_lists)}\n")
-    print(f"{experimenting(SelectionSort.selection_sort, custom_lists)}\n")
-    print(f"{experimenting(ShellSort.shell_sort, custom_lists)}\n")
+    add_logs.add_data_to_log(experimenting(InsertionSort.insertion_sort, custom_lists), 'insertion_sort')
+    add_logs.add_data_to_log(experimenting(SelectionSort.selection_sort, custom_lists), 'selection_sort')
+    add_logs.add_data_to_log(experimenting(ShellSort.shell_sort, custom_lists), 'shell_sort')
+   
