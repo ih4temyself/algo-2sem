78b7da46039d56460103adcd48997bc886a0aac2
noobxzx <148211453+noobxzx@users.noreply.github.com>
2024-03-01 19:12:15 +0200

    emotions

--- section6/emotional_sort.py
+++ section6/emotional_sort.py
@@ -1,20 +1,9 @@
-def selection_sort(array, order):
-    for i in range(len(array)):
-        min_index = i
-        for j in range(i + 1, len(array)):
-            if order[array[min_index]] > order[array[j]]:
-                min_index = j
-        array[i], array[min_index] = array[min_index], array[i]
-    return array
-        
 def sort_emotions(array, mode): 
+    EMOTES_ORDER = {':D':0, ':)':1, ":|":2, ':(':3, 'T_T':4}
     if mode:
-        EMOTES_ORDER = {':D':0, ':)':1, ":|":2, ':(':3, 'T_T':4}
-        return selection_sort(array, EMOTES_ORDER)
-        
-    else: 
-        EMOTES_ORDER_REV = {':D':4, ':)':3, ":|":2, ':(':1, 'T_T':0}
-        return selection_sort(array, EMOTES_ORDER_REV)
+         return sorted(array, key=lambda x: EMOTES_ORDER[x])
+    else:
+        return sorted(array, key=lambda x: EMOTES_ORDER[x], reverse=True)
 
 if __name__ == '__main__':
     arr = [':D', ':|', ':)', ':(', ':D']
