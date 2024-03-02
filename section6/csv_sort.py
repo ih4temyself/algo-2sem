890b4cd421708bc8237a57a6a5bd47432090c6f9
noobxzx <148211453+noobxzx@users.noreply.github.com>
2024-03-02 09:52:07 +0200

    csv sort

---
+++ section6/csv_sort.py
@@ -0,0 +1,19 @@
+def sort_csv_columns(csv_string):
+    lines = csv_string.strip().split('\n')
+    columns = lines[0].split(';')
+    sorted_columns = sorted(columns, key=lambda ln: ln.lower())
+
+    sorted_csv = []
+    sorted_csv.append(';'.join(sorted_columns))
+
+    for line in lines[1:]:
+        values = line.split(';')
+        sorted_values = [values[columns.index(col)] for col in sorted_columns]
+        sorted_csv.append(';'.join(sorted_values))
+    
+    return '\n'.join(sorted_csv)
+
+
+csv_input = 'myjinxin2015;raulbc777;smile67;Dentzil;SteffenVogel_79\n17945;10091;10088;3907;10132\n2;12;13;48;11'
+sorted_csv_output = sort_csv_columns(csv_input)
+print(sorted_csv_output)
