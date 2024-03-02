dcf614d6ca4eae5096e18cf66060b9946e38bcd4
noobxzx <148211453+noobxzx@users.noreply.github.com>
2024-03-02 09:04:30 +0200

    first_n_smallest

---
+++ section6/nsmallest.py
@@ -0,0 +1,16 @@
+def first_n_smallest(arr, n):
+    min_order_dict = {}
+
+    for _ in range(n):
+        new_min = min(arr)
+        new_min_pos = arr.index(new_min)
+        min_order_dict[new_min_pos] = new_min
+        arr[new_min_pos] = float('inf')
+
+    mins_list = sorted(min_order_dict.items())
+    return [tup[1] for tup in mins_list]
+ 
+
+if __name__ == "__main__":
+    test_list = [1,2,3,4,1]
+    print(first_n_smallest(test_list, 3))
